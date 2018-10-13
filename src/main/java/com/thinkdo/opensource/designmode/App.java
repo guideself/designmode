76b38f277b43417f6e07630b28054b96d8092d91
guideself <welcomezs@sina.com>
2018-10-13 17:39:45 +0800

    1

---
+++ src/main/java/com/thinkdo/opensource/designmode/App.java
@@ -0,0 +1,13 @@
+package com.thinkdo.opensource.designmode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
