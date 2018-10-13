1315df1777d7ff9450b5127fc3fdd9335dda68a6
guideself <welcomezs@sina.com>
2018-10-13 18:00:01 +0800

    简单工厂模式

--- src/main/java/com/thinkdo/opensource/designmode/App.java
+++ src/main/java/com/thinkdo/opensource/designmode/App.java
@@ -1,13 +1,18 @@
 package com.thinkdo.opensource.designmode;
 
+import junit.framework.TestCase;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends TestCase
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+    
+    
+    
 }
